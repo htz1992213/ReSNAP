b04c1bf195fe3eb6f51503d84e58c7cf95157eab
Tingzheng Hou <tingzheng_hou@berkeley.edu>
2020-05-03 20:38:16 -0700

    add main to example

--- examples/example_make_feature.py
+++ examples/example_make_feature.py
@@ -1,11 +1,18 @@
-from re_ml.core.make_feature import FeatureMaker
 import os
+
+from re_ml.core.make_feature import FeatureMaker
 
 XSF_DIR = os.path.join(os.path.dirname(__file__), "xsf_files")
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), "data_files")
 
-fm = FeatureMaker(XSF_DIR, DATA_DIR,
-                  {"rcutfac": 2.0, "rfac0": 0.94, "twojmax": 10, "R_1": 0.9},
-                  mode="structure", screen=False, num_of_samples=10)
-fm.save_samples(DATA_DIR, "re.mat")
+
+def example_maker():
+    fm = FeatureMaker(XSF_DIR, DATA_DIR,
+                      {"rcutfac": 2.0, "rfac0": 0.94, "twojmax": 10, "R_1": 0.9},
+                      mode="structure", screen=False, num_of_samples=10)
+    fm.save_samples(DATA_DIR, "re.mat")
+
+
+if __name__ == '__main__':
+    example_maker()
--- examples/example_train_potential.py
+++ examples/example_train_potential.py
@@ -1,9 +1,16 @@
-from re_ml.core.train_potential import PotentialTrainer
-import numpy as np
 import os
+import numpy as np
+
+from re_ml.core.train_potential import PotentialTrainer
 
 DATA_DIR = os.path.dirname(__file__)
 
-pt = PotentialTrainer(os.path.join(DATA_DIR, "re.mat"), "RIDGE")
-error = pt.cross_validation(np.array([0, 0.01, 0.1, 1]), plot_image=False)
-pt.make_potential(DATA_DIR, alpha=0.0)
+
+def example_maker():
+    pt = PotentialTrainer(os.path.join(DATA_DIR, "re.mat"), "RIDGE")
+    pt.cross_validation(np.array([0, 0.01, 0.1, 1]), plot_image=False)
+    pt.make_potential(DATA_DIR, alpha=0.0)
+
+
+if __name__ == '__main__':
+    example_maker()
