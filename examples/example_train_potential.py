88bef50d5d170938df8fde9b7fa9c89170a5a3c5
Tingzheng Hou <tingzheng_hou@berkeley.edu>
2020-05-07 02:37:13 -0700

    change example name

--- examples/example_train_potential.py
+++ examples/example_train_potential.py
@@ -6,11 +6,11 @@ from resnap.core.train_potential import PotentialTrainer
 DATA_DIR = os.path.dirname(__file__)
 
 
-def example_maker():
+def example_trainer():
     pt = PotentialTrainer(os.path.join(DATA_DIR, "re.mat"), "RIDGE")
     pt.cross_validation(np.array([0, 0.01, 0.1, 1]), plot_image="atom")
     pt.make_potential(DATA_DIR, alpha=0.0)
 
 
 if __name__ == '__main__':
-    example_maker()
+    example_trainer()
